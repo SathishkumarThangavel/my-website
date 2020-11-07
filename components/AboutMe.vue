08e486c87b21768f3d6be7a6fe6345415142e4c3
SathishkumarThangavel <2012sathishkumarsrec@gmail.com>
2020-11-07 13:18:37 +0530

    Build Home page

---
+++ components/AboutMe.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="section-container"> 
+        <div class="introduction"></div>
+        <h2 id="work-experience">Work Experience</h2>
+        <!-- Freshworks -->
+        <div class="experience">
+            <div class="company">
+                <img src="~/assets/freshworks.svg" class="logo" alt="">
+                <h3>Freshworks</h3>
+                <a target="_blank" href="https://www.freshworks.com" rel="noopener noreferrer">
+                    <img src="~/assets/openlink.svg" class="open-external-link" alt="">
+                </a>
+            </div>
+            <div class="period">
+                Chennai, India - (Oct 2019 - Present)
+            </div>
+            <div class="project">
+                <h3>Tech team - Platform</h3>
+                <p>Enhance the developer platform, Requirement analysis, design, develop, review codes, support developer community and organization maintained opensource contribution</p>
+            </div>
+        </div>
+        <hr>
+        <!-- Zoho -->
+        <div class="experience">
+            <div class="company">
+                <img src="~/assets/zoho.png" class="logo" style="width:100px;height:auto" alt="Zoho">
+                <h3>Corporation Pvt. Ltd</h3>
+                <a target="_blank" href="https://www.zoho.com" rel="noopener noreferrer">
+                    <img src="~/assets/openlink.svg" class="open-external-link" alt="">
+                </a>
+            </div>
+            <div class="period">
+                Chennai, India - (May 2016 - Oct 2019)
+            </div>
+            <div class="project">
+                <h3>UI Team - Product</h3>
+                <p> Enhance Product Features, add unit and integration tests, peer code reviews, requirement gathering, Mentor new team member  </p>
+                <h3>UI Team - Marketing</h3>
+                <p> Build Customer experiencing marketing websites, enhance performance, improve SEO, make website responsive and smooth transitions </p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'about-me',
+}
+</script>
+<style scoped>
+    .section-container {
+        max-width: 50%;
+        margin: 50px auto;
+    }
+    h2 {
+        margin: 24px 0;
+    }
+    .experience {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+    .company {
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        margin-top: 0.5rem;
+        margin-bottom: 0.5rem;
+    }
+    .logo {
+        width: 40px;
+        height: 40px;
+        margin-right: 0.5rem;
+    }
+    h3 {
+        font-size: 24px;
+    }
+    .open-external-link {
+        width: 12px;
+        height: 12px;
+        margin-left: 15px;
+        opacity: 0.3;
+    }
+    .open-external-link:hover {
+        opacity: 1;
+    }
+    .period {
+        font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        margin-bottom: 0.5rem;
+        color:#6d6b6b;
+    }
+    .project h3 {
+        font-size: 18px
+    }
+    .project p {
+        font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        margin-top: 5px;
+        margin-bottom: 10px;
+        line-height: 1.5em;
+    }
+    hr {
+        border-width: 0px 0px 0.0625rem;
+        opacity: 0.6;
+        margin-top: 0.5rem;
+        margin-bottom: 0.5rem;
+        border-color: rgb(74, 85, 104);
+    }
+</style>
---
+++ components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="footer">
+        <div class="contact">
+        <a href="https://twitter.com/ITsathishkumar">
+            <img src="~/assets/twitter.svg" alt="twitter">
+        </a>
+        <a href="https://github.com/SathishkumarThangavel">
+            <img src="~/assets/github.svg" alt="github">
+        </a>
+        <a href="https://www.linkedin.com/in/sathishkumarthangavel/">
+            <img src="~/assets/linkedin.svg" alt="linkedin">
+        </a>
+        </div>
+        <div class="powered"> © Sathishkumar Thangavel. Powered by <a href="https://nuxtjs.org/">Nuxtjs</a></div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'footer',
+}
+</script>
+<style>
+.footer {
+    margin-top: 40px;
+    max-width: 50%;
+    margin: 50px auto;
+}
+img {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+}
+.contact {
+    text-align: center;
+}
+.powered {
+    margin-top: 10px;
+    text-align: center
+}
+</style>
